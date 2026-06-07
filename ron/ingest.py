002ba3ee80fd012837d6269939db1b6282be64e8
Awez <awezacademic@gmail.com>
2026-06-07 12:20:41 +0530

    ron: ingest pipeline working, vector store generated

---
+++ ron/ingest.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
+
+loader = DirectoryLoader(
+    'knowledge_base/',
+    glob = "**/*.md",
+    loader_cls=TextLoader
+)
+
+documents = loader.load()
+print(f'Loader {len(documents)} documents successfully...')
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50
+)
+
+chunks = splitter.split_documents(documents)
+print(f'Splitted into {len(chunks)} chunks...')
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vector_store = FAISS.from_documents(chunks,embeddings)
+
+vector_store.save_local("vector_store")
+print("Vector STore saved...")
