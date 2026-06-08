9677c392dca1bba31564c7ea78743ca784b2e764
Awez <awezacademic@gmail.com>
2026-06-08 13:25:16 +0530

    fix: static files, whitenoise, collectstatic in Docker

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 RUN python ron/ingest.py
+RUN python manage.py collectstatic --noinput
 
 EXPOSE 8000
 
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
